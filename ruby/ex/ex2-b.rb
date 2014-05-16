9293bf76ff377c25073ddac25f1177a7d0597422
Joao Marinho <joao.marinho@codus.com.br>
2014-05-16 11:47:37 -0300

    Arrumado o erro do ex2

--- ruby/ex/ex2-b.rb
+++ ruby/ex/ex2-b.rb
@@ -16,9 +16,12 @@ def rps_game_winner(game)
   raise WrongNumberOfPlayersError unless game.length == 2
   raise NoSuchStrategyError unless strategyExists?(game[0][1]) && strategyExists?(game[1][1])
 
-  game[1] if secondIsBetter?(game[0][1], game[1][1])
-
-  game[0]
+  if secondIsBetter?(game[0][1], game[1][1])
+    game[1] 
+  else 
+    game[0]
+  end
+  
 end
 
 def is_tournament?(game)
