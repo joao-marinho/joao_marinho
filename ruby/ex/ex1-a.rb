edf1d960dc7b31e488bbb677632e5d9f8292905d
Codus Tecnologia <codustecnologia@coduss-mbp-2.home>
2014-05-21 19:22:49 -0300

    Retirado os testes que estavam dentro do arquivo ex1-a.rb

--- ruby/ex/ex1-a.rb
+++ ruby/ex/ex1-a.rb
@@ -1,7 +1,3 @@
 def palindrome?(string)
   (justLetters = string.downcase.scan(/([a-z]+)/).join) == justLetters.reverse
 end
-
-p palindrome?("A man, a plan, a canal -- Panama")
-p palindrome?("Madam, I'm Adam!")
-p palindrome?("Abracadabra") 
