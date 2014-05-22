3445722314e3dc233b0350f9adc90667b6010ca6
Joao Marinho <joao.marinho@codus.com.br>
2014-05-22 09:49:09 -0300

    Adicionado os describes em ex2-a_spec.rb

--- ruby/ex/spec/ex2-a_spec.rb
+++ ruby/ex/spec/ex2-a_spec.rb
@@ -17,3 +17,47 @@ describe "strategyExists?" do
     strategyExists?("X").should_not be_true
   end
 end
+
+describe "secondIsBetter?" do
+  it "return true for R vs P" do
+    secondIsBetter?("R", "P").should be_true
+  end
+
+  it "return true for P vs S" do
+    secondIsBetter?("P", "S").should be_true
+  end
+
+  it "return true for S vs R" do
+    secondIsBetter?("S", "R").should be_true
+  end
+
+  it "return false when the strategies are equal" do
+    secondIsBetter?("S", "S").should_not be_true
+  end
+
+  it "return false when the first strategy is better" do
+    secondIsBetter?("S", "P").should_not be_true
+  end
+end
+
+describe "rps_game_winner" do
+
+    it "raise WrongNumberOfPlayersError when the number of players are different of 2" do
+      game = [["P"],["L"],["Y"]];
+      expect { rps_game_winner(game) }.to raise_error(WrongNumberOfPlayersError);
+    end
+
+    it "raise NoSuchStrategyError when the strategy does not exist" do
+      game = [["Armando", "P"],["Antonio", "X"]];
+      expect { rps_game_winner(game) }.to raise_error(NoSuchStrategyError);
+    end
+
+    it "return the winner" do
+      winner = ["Joao", "S"]
+      loser = ["Batista", "P"]
+      game = [winner, loser];
+
+      rps_game_winner(game).should be(winner);
+    end
+
+  end
