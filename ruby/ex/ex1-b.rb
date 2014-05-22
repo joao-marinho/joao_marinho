7f45137712253bc0e77d343d69008e51d78560c1
Joao Marinho <joao.marinho@codus.com.br>
2014-05-22 08:47:42 -0300

    Retirado as chamdas da funcao count_words de dentro do arquivo ex1-b.rb

--- ruby/ex/ex1-b.rb
+++ ruby/ex/ex1-b.rb
@@ -1,6 +1,3 @@
 def count_words(string)
   string.downcase.scan(/([a-z]+)/).each_with_object Hash.new do |(k), hash| hash[k] = hash[k] ? hash[k] + 1 : 1 end
 end
-
-p count_words("A man, a plan, a canal -- Panama");
-p count_words "Doo bee doo bee doo"
