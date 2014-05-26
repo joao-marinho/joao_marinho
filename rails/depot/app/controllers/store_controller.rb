83e1e8848e2594fca505c63c182e8247f133954f
Joao Marinho <joao.marinho@codus.com.br>
2014-05-26 14:44:45 -0300

    (PlayTime) Adicionado o atributo counter na sessao que mostrado no index

--- rails/depot/app/controllers/store_controller.rb
+++ rails/depot/app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    session[:counter] ||= 0
+    session[:counter] += 1
+    @counter = session[:counter];
+
   end
 end
