52fb66741115bf43f49cbfc6b81b450579b1ad20
Joao Marinho <joao.marinho@codus.com.br>
2014-05-28 11:07:04 -0300

    Adicionado Sessions#create

--- rails/depot/app/controllers/sessions_controller.rb
+++ rails/depot/app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password combination"
   end
 
   def destroy
