2b0a17db210c1d9124f43463b29ed9d172861c04
Joao Marinho <joao.marinho@codus.com.br>
2014-05-28 11:22:59 -0300

    Adicionado o formulario de loggin; Adicionado Session#destroy

--- rails/depot/app/controllers/sessions_controller.rb
+++ rails/depot/app/controllers/sessions_controller.rb
@@ -9,8 +9,11 @@ class SessionsController < ApplicationController
       redirect_to admin_url
     else
       redirect_to login_url, alert: "Invalid user/password combination"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to store_url, notice: "Logged out"
   end
 end
