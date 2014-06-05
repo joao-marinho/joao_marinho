1459840e49014fc5f0a65af040ce89c5be2725a7
Joao Marinho <joao.marinho@codus.com.br>
2014-06-05 13:30:58 -0300

    Adicionado o controller de User

---
+++ rails/loja/app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_param)
+    @user.save
+    redirect_to @user
+  end
+
+  def edit
+
+  end
+
+  def update
+    @user.update(user_param)
+    redirect_to @user
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_param
+    params.require(:user).permit(:name, :password, :group)
+  end
+
