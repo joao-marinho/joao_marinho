2e06a356f0e8552cd2d386759caa93111707990d
Joao Marinho <joao.marinho@codus.com.br>
2014-06-06 10:38:15 -0300

    Adicionado a validacao dos dados do produto.

--- rails/loja/app/models/product.rb
+++ rails/loja/app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
+  validates :description, :image_url, :price, :title, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
   has_and_belongs_to_many :categories
 end
